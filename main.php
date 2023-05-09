174f98b85cfda9b9e68794515902a1f5b28c43bd
titusse3 <61415485+titusse3@users.noreply.github.com>
2023-05-09 22:09:17 +0200

    ajout début

---
+++ main.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>Ma page perso</title>
+    <link rel="stylesheet" href="./css/header_style.css" />
+</head>
+
+<body>
+    <header>
+        <img src="/img/taup.png" alt="Logo du site taupe-hotel" width="75px" height="75px"/>
+        <h1>Taup'Hotel</h1>
+        <button class="recherche-button">Recherche</button>
+        <button>Sign in</button>
+        <button>Sign up</button>
+    </header>
+</body>
+
+</html>
