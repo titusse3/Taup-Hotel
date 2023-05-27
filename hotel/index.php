8d4486558e46c308e9c54b8415eb1f58556e91a4
Lurgrid <Lurgrid@gmail.com>
2023-05-27 19:54:29 +0200

    ..

--- hotel/index.php
+++ hotel/index.php
@@ -7,42 +7,53 @@
     <link rel="stylesheet" href="../src/css/hotel.css" />
     <link rel="stylesheet" href="../src/css/article.css" />
     <script src="http://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script defer src="app.js"></script>
 </head>
 
 <body>
-    <?php 
-        include_once '../src/.header.php';
-        header_show('../');
-    ?>
+    <header class="h-off">
+        <a class="site-logo" href="../">
+            <img src="../src/img/taup.png" alt="Logo du site taupe-hotel" width="75px" height="75px" />
+            <h1>Taup'Hotel</h1>
+        </a>
+        <a id="recherche-button" href="../search">
+            Recherche
+            <img src="../src/img/search.svg" alt="Loupe de recherche" />
+        </a>
+        <div class="sig-iu">
+            <a href="../signin">Sign in</a>
+            <a href="../signin">Sign up</a>
+        </div>
+    </header>
     <main>
         <section class="top-section">
             <div class="info">
-                <h2 id="title-hotel">Le bourquer</h2>
+                <h2 id="title-hotel"></h2>
                 <div class="info-1">
                     <div class="localisation">
-                        <img src="../src/img/location.svg"/>
-                        <h4 class="sub-info">Rouen, Normandie, 15 rue du proute</h4>
+                        <img src="../src/img/location.svg" />
+                        <h4 class="sub-info"></h4>
                     </div>
                     <div class="notation">
-                        <img src="../src/img/heart.svg"/>
-                        <img src="../src/img/heart.svg"/>
-                        <img src="../src/img/heart.svg"/>
-                        <img src="../src/img/heart.svg"/>
-                        <img src="../src/img/heart.svg"/>
-                        <h4 class="sub-info">4.9</h4>
+                        <img src="../src/img/heart.svg" />
+                        <img src="../src/img/heart.svg" />
+                        <img src="../src/img/heart.svg" />
+                        <img src="../src/img/heart.svg" />
+                        <img src="../src/img/heart.svg" />
+                        <h4 class="sub-info"></h4>
                     </div>
                 </div>
             </div>
             <div class="photo-hotel">
                 <div class="title">
                     <div class="banque-img">
-                        <img id="img1" src="../src/img/img-h/1.jpg"/>
+                        <img id="img1" />
                         <div class="grid-img">
-                            <img class="img-grid" src="../src/img/img-h/2.jpg"/>
-                            <img class="img-grid" src="../src/img/img-h/3.jpg"/>
-                            <img class="img-grid" src="../src/img/img-h/4.jpg"/>
-                            <img class="img-grid" src="../src/img/img-h/5.jpg"/>
-                         </div>
+                            <img class="img-grid" />
+                            <img class="img-grid" />
+                            <img class="img-grid" />
+                            <img class="img-grid" />
+                        </div>
                     </div>
                 </div>
             </div>
@@ -50,11 +61,11 @@
         <section class="descrition">
             <div class="desc">
                 <h3>Description</h3>
-                <p class="info-desc">Hotel de malade mental de baiser c'est vreument bien. Cette élégante église transformée en 1925 en style gothique français dispose de vitraux originaux, d'une charmante chambre d'hôtes + baignoire avec baignoire à pied/douche à griffes, lustre, climatisation et cuisine partagée. Savourez un café matinal ensoleillé dans le jardin, un verre de vin ou lisez au coin du feu dans le salon les nuits froides. Une oasis de paix au milieu de la ville, confortable et magnifique. Assez privé, noussommes rarement en bas et cuisinons 3-4 verges/semaine. À égale distance de la plage, d'Hollywood</p>
+                <p class="info-desc"></p>
             </div>
             <div class="owner">
                 <h3>Gérant</h3>
-                <p class="info-desc">Pirre de la Rue Verte</p>
+                <p class="info-desc"></p>
             </div>
         </section>
         <section class="chambre-section">
@@ -65,10 +76,10 @@
                 <div class="filter">
                     Trier par
                     <select id="trie-button">
-                        <option selected="selected" disabled>Prix</option> 
+                        <option selected="selected" disabled>Prix</option>
                         <option>Peu importe</option>
-                        <option>↑ (croissant)</option> 
-                        <option>↓ (décroissant)</option> 
+                        <option>↑ (croissant)</option>
+                        <option>↓ (décroissant)</option>
                     </select>
                     <select id="etoile-button">
                         <option selected="selected" disabled>Notation</option>
@@ -80,14 +91,14 @@
                         <option>★★★★★</option>
                     </select>
                     <select id="room-button">
-                        <option selected="selected" disabled>Etat</option> 
-                        <option>Libre</option> 
+                        <option selected="selected" disabled>Etat</option>
+                        <option>Libre</option>
                         <option>Réservé</option>
                     </select>
                     <select>
-                        <option selected="selected" disabled>Type de Chambre</option> 
-                        <option>Dortoire</option> 
-                        <option>Chambre seule</option> 
+                        <option selected="selected" disabled>Type de Chambre</option>
+                        <option>Dortoire</option>
+                        <option>Chambre seule</option>
                     </select>
                 </div>
                 <?php
