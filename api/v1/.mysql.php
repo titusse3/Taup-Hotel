ea73cf58b0024fca3cdbc62521b5420a77cc98e8
Lurgrid <Lurgrid@gmail.com>
2023-05-28 23:19:04 +0200

    ..

--- api/v1/.mysql.php
+++ api/v1/.mysql.php
@@ -1,8 +1,8 @@
 <?php
     define('HOST_DB', 'localhost');
     define('NAME_DB', 'TAUPEH');
-    define('USER_DB', 'lurgrid');
-    define('PWD_DB', 'Lurgrid17@');
+    define('USER_DB', '');
+    define('PWD_DB', '');
     function logDB() {
         return new PDO('mysql:host=' . HOST_DB . ';dbname=' . NAME_DB, USER_DB, 
             PWD_DB);
